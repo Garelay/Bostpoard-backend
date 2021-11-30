bf447f8aa5874cd70ef8e56ca66ef991050ad16b
Konstantin Mochalov <constantin.mochalov@gmail.com>
2021-11-30 07:46:55 +0300

    added all the necessery post routes

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,10 +1,9 @@
-import express from "express"
 import mongoose from "mongoose"
-import postMessage from "../models/postMessage.js";
+import postMessage from "../models/postMessage.js"
 
 export const getPosts = async (req,res)=>{
     try {
-        const postMessages = await postMessage.find();
+        const postMessages = await postMessage.find()
         console.log(postMessages);
         res.status(200).json(postMessages)
     } catch (error) {
@@ -17,10 +16,33 @@ export const createPost = async (req,res) =>{
     const newPost = new postMessage(post);
     try {
         await newPost.save()
-        // console.log(newPost);
         res.status(201).json(newPost)
         
     } catch (error) {
         res.status(409).json({message: error.message})
     }
+}
+
+export const likePost = async(req,res) =>{
+    const {id} = req.params
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`)
+    const post = await postMessage.findById(id)
+    const updatedPost = await postMessage.findByIdAndUpdate(id, { likeCount: post.likeCount + 1 }, { new: true }); 
+    res.json(updatedPost)
+}
+
+export const deletePost = async(req,res) =>{
+    const {id} = req.params;
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`)
+    const post = await postMessage.findByIdAndRemove(id)
+    res.json({message: "Post deleted succesfully."})
+}
+
+export const updatePost = async(req,res) =>{
+    const {id} = req.params
+    const { title, message, creator, selectedFile, tags } = req.body
+    if (!mongoose.Types.ObjectId.isValid(id)) return res.status(404).send(`No post with id: ${id}`)
+    const updatedPost = { creator, title, message, tags, selectedFile, _id: id }
+    await postMessage.findByIdAndUpdate(id, updatePost)
+    res.json(updatedPost)
 }
